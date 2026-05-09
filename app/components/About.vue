c77ba334ad69e46fa6e65ed045e2d3088a2611da
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-05-09 17:32:51 +0200

    feat: added about

---
+++ app/components/About.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="about section" id="about">
+    <h2 class="section_title">About Me</h2>
+    <span class="section_subtitle">My introduction</span>
+
+    <div class="about_container container grid">
+      <img src="/photo4.jpg" alt="photo4" class="about_img" />
+
+      <div class="about_data">
+        <p class="about_description">
+          Web developer, with extensive knowledge and years of experience,
+          working in web technologies and ui / ux design, delivering quality
+          work.
+        </p>
+
+        <div class="about_info">
+          <div>
+            <span class="about_info-title">3+</span>
+            <span class="about_info-name"
+              >Years <br />
+              experience</span
+            >
+          </div>
+
+          <div>
+            <span class="about_info-title">30+</span>
+            <span class="about_info-name"
+              >Projects <br />
+              done</span
+            >
+          </div>
+
+          <div>
+            <span class="about_info-title">4+</span>
+            <span class="about_info-name"
+              >Years <br />
+              experience</span
+            >
+          </div>
+        </div>
+
+        <div class="about_buttons">
+          <a
+            href=""
+            class="button button-flex"
+            download="Curriculum Vitae - Ziya Sensoy"
+            >Download CV <i class="uil uil-download-alt button_icon"></i
+          ></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,5 +2,6 @@
     <div>
         <Header />
         <Hero />
+        <About />
     </div>
 </template>
