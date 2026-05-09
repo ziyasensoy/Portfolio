710469383863a36dd9246324d02b8c1434be3f60
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-05-09 17:23:14 +0200

    feat: import images and CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://unicons.iconscout.com/release/v4.0.0/css/line.css'
+        }
+      ]
+    }
+  }
 })
