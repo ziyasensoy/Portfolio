c55284bc76542d4efb8506b0bf1f0a410d0eddb0
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-05-19 23:16:30 +0200

    style: contact form centered

--- app/components/Contact.vue
+++ app/components/Contact.vue
@@ -1,36 +1,7 @@
 <template>
   <section class="contact section" id="contact">
-    <h2 class="section_title">{{ $t("Contact.title") }}</h2>
-    <span class="section_subtitle">{{ $t("Contact.subtitle") }}</span>
-
     <div class="contact_container container grid">
-      <div>
-        <div class="contact_information">
-          <i class="uil uil-phone contact_icon"></i>
-
-          <div>
-            <h3 class="contact_title">{{ $t("Contact.phoneTitle") }}</h3>
-            <span class="contact_subtitle">{{ $t("Contact.phone") }}</span>
-          </div>
-        </div>
-
-        <div class="contact_information">
-          <i class="uil uil-envelope contact_icon"></i>
-
-          <div>
-            <h3 class="contact_title">{{ $t("Contact.emailTitle") }}</h3>
-            <span class="contact_subtitle">{{ $t("Contact.email") }}</span>
-          </div>
-        </div>
-        <div class="contact_information">
-          <i class="uil uil-map-marker contact_icon"></i>
-
-          <div>
-            <h3 class="contact_title">{{ $t("Contact.locationTitle") }}</h3>
-            <span class="contact_subtitle">{{ $t("Contact.location") }}</span>
-          </div>
-        </div>
-      </div>
+      <h2 class="section_title">{{ $t("Contact.title") }}</h2>
 
       <form
         action="https://formsubmit.co/zcsensoy@gmail.com"
@@ -39,11 +10,15 @@
       >
         <div class="contact_inputs grid">
           <div class="contact_content">
-            <label for="" class="contact_label">{{ $t("Contact.Form.name") }}</label>
+            <label for="" class="contact_label">{{
+              $t("Contact.Form.name")
+            }}</label>
             <input type="text" name="name" class="contact_input" required />
           </div>
           <div class="contact_content">
-            <label for="" class="contact_label">{{ $t("Contact.Form.email") }}</label>
+            <label for="" class="contact_label">{{
+              $t("Contact.Form.email")
+            }}</label>
             <input
               type="email"
               name="email"
@@ -54,11 +29,15 @@
           </div>
         </div>
         <div class="contact_content">
-          <label for="" class="contact_label">{{ $t("Contact.Form.subject") }}</label>
+          <label for="" class="contact_label">{{
+            $t("Contact.Form.subject")
+          }}</label>
           <input type="text" name="subject" class="contact_input" />
         </div>
         <div class="contact_content">
-          <label for="" class="contact_label">{{ $t("Contact.Form.message") }}</label>
+          <label for="" class="contact_label">{{
+            $t("Contact.Form.message")
+          }}</label>
           <textarea
             cols="0"
             rows="7"
