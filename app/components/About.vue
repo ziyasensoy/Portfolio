cbd03f83cfa38df2b59747ee87e50ce22d21f480
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-06-10 18:08:05 +0200

    feat: about page: changed description, photo, made responsive and more

--- app/components/About.vue
+++ app/components/About.vue
@@ -7,40 +7,60 @@
       </h2>
 
       <div class="about_container container grid">
-        <img src="/photo4.jpg" alt="photo4" class="about_img" />
+        <img src="/about_photo.png" alt="about_photo" class="about_img" />
 
         <div class="about_data">
-          <p class="about_description">
-            <i18n-t keypath="About.description">
-              <template #hva>
-                <a
-                  class="about_link"
-                  href="https://www.hva.nl"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {{ $t("General.hva") }}
-                </a>
-              </template>
+          <div class="about_description">
+            <p>
+              <i18n-t keypath="About.Description.line1">
+                <template #hva>
+                  <a
+                    class="about_description-links"
+                    href="https://www.hva.nl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {{ $t("General.hva") }}
+                  </a>
+                </template>
+              </i18n-t>
+            </p>
 
-              <template #altura>
-                <a
-                  class="about_link"
-                  href="https://altura.io"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {{ $t("General.altura") }}
-                </a>
-              </template>
-            </i18n-t>
-          </p>
-          <div class="home_socials">
+            <p>
+              <i18n-t keypath="About.Description.line2">
+                <template #altura>
+                  <a
+                    class="about_description-links"
+                    href="https://altura.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {{ $t("General.altura") }}
+                  </a>
+                </template>
+              </i18n-t>
+            </p>
+
+            <p>
+              <i18n-t keypath="About.Description.line3">
+                <template #email>
+                  <a
+                    href="mailto:zcsensoy@gmail.com"
+                    target="_blank"
+                    class="about_description-links"
+                  >
+                    {{ $t("About.Description.email") }}
+                  </a>
+                </template>
+              </i18n-t>
+            </p>
+          </div>
+          <div class="socials">
             <a
               href="/resume.pdf"
               target="_blank"
               rel="noopener noreferrer"
-              class="home_resume-download"
+              class="resume_download-button"
             >
               {{ $t("General.resume") }}
               <i class="uil uil-file-download"></i>
@@ -51,7 +71,7 @@
             <a
               href="https://www.linkedin.com/in/ziyasensoy/"
               target="_blank"
-              class="home_social-icon"
+              class="social_icon"
             >
               <i class="uil uil-linkedin-alt"></i>
               {{ $t("General.Socials.linkedin") }}
@@ -62,7 +82,7 @@
             <a
               href="https://github.com/ziyasensoy"
               target="_blank"
-              class="home_social-icon"
+              class="social_icon"
             >
               <i class="uil uil-github-alt"></i>
               {{ $t("General.Socials.github") }}
@@ -73,16 +93,10 @@
             <a
               href="mailto:zcsensoy@gmail.com"
               target="_blank"
-              class="home_social-icon"
+              class="social_icon"
             >
               <i class="uil uil-envelope"></i>
             </a>
-
-            <span class="social_divider">|</span>
-
-            <NuxtLink to="/about" class="home_social-icon"
-              >{{ $t("Hero.more") }} <i class="uil uil-arrow-right"></i>
-            </NuxtLink>
           </div>
         </div>
       </div>
