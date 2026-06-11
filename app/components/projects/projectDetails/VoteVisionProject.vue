5fd5a823c4b4463a954f514f19032ca09797961d
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-06-12 00:43:48 +0200

    Feat: added content to votevision detail page

--- app/components/projects/projectDetails/VoteVisionProject.vue
+++ app/components/projects/projectDetails/VoteVisionProject.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="project-content">
-    <p>VoteVision project content.</p>
+    <p>{{ $t("Projects.items.votevision.content.intro1") }}</p>
+    <p>{{ $t("Projects.items.votevision.content.intro2") }}</p>
+
+    <h2>{{ $t("Projects.items.votevision.content.contributionsTitle") }}</h2>
+
+    <p>{{ $t("Projects.items.votevision.content.contribution1") }}</p>
+    <p>{{ $t("Projects.items.votevision.content.contribution2") }}</p>
+
+    <h3>{{ $t("Projects.items.votevision.content.landingPage") }}</h3>
+    <img
+      src="/projects/votevision/votevision_landingpage.png"
+      alt="Votevision landing page"
+    />
+
+    <h3>{{ $t("Projects.items.votevision.content.resultsPage") }}</h3>
+    <img
+      src="/projects/votevision/votevision_resultspage_1.png"
+      alt="Votevision results page 1"
+    />
+    <img
+      src="/projects/votevision/votevision_resultspage_2.png"
+      alt="Votevision results page 2"
+    />
+
+    <h3>{{ $t("Projects.items.votevision.content.quizPage") }}</h3>
+    <img
+      src="/projects/votevision/votevision_quiz.png"
+      alt="Votevision quiz page"
+    />
   </div>
 </template>
