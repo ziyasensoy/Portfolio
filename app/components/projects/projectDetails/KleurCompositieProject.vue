cc2bc1ceb3f612df2d7f26a92f049e7dc35c3745
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-06-13 20:45:02 +0200

    Feat: color and composition project details

--- app/components/projects/projectDetails/KleurCompositieProject.vue
+++ app/components/projects/projectDetails/KleurCompositieProject.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="project-content">
-    <p>Kleur & Compositie project content.</p>
+    <p class="project-content-grade">{{ t("Projects.items.kleurCompositie.content.grade") }}</p>
+
+    <p>{{ t("Projects.items.kleurCompositie.content.intro1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.intro2") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.intro3") }}</p>
+
+    <h3>{{ t("Projects.items.kleurCompositie.content.brandingTitle") }}</h3>
+    <p>{{ t("Projects.items.kleurCompositie.content.branding1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.branding2") }}</p>
+
+    <h3>{{ t("Projects.items.kleurCompositie.content.hierarchieTitle") }}</h3>
+    <p>{{ t("Projects.items.kleurCompositie.content.hierarchie1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.hierarchie2") }}</p>
+
+    <h3>{{ t("Projects.items.kleurCompositie.content.datavisualisatieTitle") }}</h3>
+    <p>{{ t("Projects.items.kleurCompositie.content.datavisualisatie1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.datavisualisatie2") }}</p>
+
+    <h3>{{ t("Projects.items.kleurCompositie.content.figmatechniektitle") }}</h3>
+    <p>{{ t("Projects.items.kleurCompositie.content.figmatechniek1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.figmatechniek2") }}</p>
+
+    <h3>{{ t("Projects.items.kleurCompositie.content.reflectieTitle") }}</h3>
+    <p>{{ t("Projects.items.kleurCompositie.content.reflectie1") }}</p>
+    <p>{{ t("Projects.items.kleurCompositie.content.reflectie2") }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
