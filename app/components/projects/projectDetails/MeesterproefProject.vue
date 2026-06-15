4e796509971f2d160b22b364b6bb113466f75d4f
Ziya Şensoy <ziya.sensoy@hva.nl>
2026-06-15 17:03:03 +0200

    feat: meesterproef project details content

--- app/components/projects/projectDetails/MeesterproefProject.vue
+++ app/components/projects/projectDetails/MeesterproefProject.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="project-content">
-    <p>Meesterproef project content.</p>
+    <p>
+      {{ t("Projects.items.meesterproef.content.intro1") }}
+    </p>
+
+    <p>
+      {{ t("Projects.items.meesterproef.content.intro2") }}
+    </p>
+
+    <h3>
+      {{ t("Projects.items.meesterproef.content.myFascinationTitle") }}
+    </h3>
+    <p>
+      {{ t("Projects.items.meesterproef.content.myFascination1") }}
+    </p>
+    <p>
+      {{ t("Projects.items.meesterproef.content.myFascination2") }}
+    </p>
+    
+    <h3>
+      {{ t("Projects.items.meesterproef.content.persoonlijkeVisieTitle") }}
+    </h3>
+    <p>
+      {{ t("Projects.items.meesterproef.content.persoonlijkeVisie1") }}
+    </p>
+    <p>
+      {{ t("Projects.items.meesterproef.content.persoonlijkeVisie2") }}
+    </p>
+    <p>
+      {{ t("Projects.items.meesterproef.content.persoonlijkeVisie3") }}
+    </p>
   </div>
 </template>
+
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
